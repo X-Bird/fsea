8784007f02d6c55d3f215660a17bf8a3eb29a14e
x-bird <x-bird@qiubs.com>
2013-10-11 16:58:44 +0800

    我不知道我在干嘛？

---
+++ src/sea-modules/company/sites/fis.baidu.com/index/js/index-debug.js
@@ -0,0 +1,3 @@
+define("company/sites/fis.baidu.com/index/js/index-debug", [ "jquery/jquery/1.10.1/jquery-debug" ], function(require, exports, module) {
+    var $ = require("jquery/jquery/1.10.1/jquery-debug");
+});
---
+++ src/sea-modules/company/sites/fis.baidu.com/index/js/index.js
@@ -0,0 +1,1 @@
+define("company/sites/fis.baidu.com/index/js/index",["jquery/jquery/1.10.1/jquery"],function(a){a("jquery/jquery/1.10.1/jquery")});
